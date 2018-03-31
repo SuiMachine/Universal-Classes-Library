adb70c79db1c20e700187e091d38141688d53dd2
Sui <link_matrix1@o2.pl>
2018-03-31 23:48:57 +0200

    Initial batch

---
+++ CodeInjection/LiveSplit/SuisCodeInjection.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using LiveSplit.ComponentUtil;
+
+namespace SuisCodeInjection
+{
+    public enum CodeInjectionResult
+    {
+        Success,
+        Failure,
+        ProcessNotFound,
+        FailedToOpenProcess,
+        FailedToGetProcAddress,
+        FailedToVirtualAlloc,
+        FailedToWriteInstructionToMemory
+    }
+
+    class CodeInjection
+    {
+        #region DLLImports
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern Int32 OpenProcess(uint dwDesiredAccess, int bInheritHandle, uint dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int CloseHandle(Int32 hObject);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern Int32 GetProcAddress(Int32 hModule, string lpProcName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern Int32 GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern Int32 VirtualAllocEx(Int32 hProcess, Int32 lpAddress, Int32 dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int WriteProcessMemory(Int32 hProcess, Int32 lpBaseAddress, byte[] buffer, uint size, int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool ReadProcessMemory(Int32 hProcess, Int32 lpBaseAddress, byte[] buffer, uint size, ref int lpNumberOfBytesToRead);
+        #endregion
+
+        /// <summary>
+        /// Result of code injection
+        /// </summary>
+        public CodeInjectionResult Result { get; }
+        private Process process;
+        private IntPtr alocAdress;
+
+        private CodeInjectionMasterContainer Container;
+
+        /// <summary>
+        /// Performs a Code Injection.
+        /// </summary>
+        /// <param name="process">Process you want to inject the code to.</param>
+        /// <param name="Container">CodeInjectionMasterContainer to inject. Create the object before the injection!</param>
+        public CodeInjection(Process process, CodeInjectionMasterContainer Container)
+        {
+            this.process = process;
+            this.Container = Container;
+
+            Validate();
+
+            Result = Inject();
+        }
+
+        /// <summary>
+        /// Checks if all injections have been closed.
+        /// </summary>
+        private void Validate()
+        {
+            foreach(var element in Container.Detours)
+            {
+                if(element.Value.MasterContainerJmpBackLocation == (IntPtr)0)
+                    throw new Exception("Injection " + element.Key + " not closed!");
+            }
+        }
+
+        /// <summary>
+        /// Used to get address of an allocated memory inside of the process.
+        /// </summary>
+        /// <returns>Address of allocated memory as Unsigned Int (uint).</returns>
+        public uint GetAllocationAddress()
+        {
+            return (uint)alocAdress;
+        }
+
+        private CodeInjectionResult Inject()
+        {
+            if(process.Id == 0)
+            {
+                return CodeInjectionResult.ProcessNotFound;
+            }
+
+            byte[] fullInjectedCode = Container.GetBytes();
+
+            IntPtr address = ExtensionMethods.AllocateMemory(process, 512);
+            if(address == null)
+            {
+                return CodeInjectionResult.FailedToVirtualAlloc;
+            }
+            alocAdress = address;
+
+            if(!ExtensionMethods.WriteBytes(process, address, fullInjectedCode))
+            {
+                return CodeInjectionResult.FailedToWriteInstructionToMemory;
+
+            }
+
+            //Replace temp return addresses with proper ones
+            foreach(var Detour in Container.Detours)
+            {
+                IntPtr LocationOfReturnJump = IntPtr.Add(alocAdress, Detour.Value.MasterContainerJmpBackLocation.ToInt32());
+                byte[] backAddy = BitConverter.GetBytes(Detour.Value.InjectionPoint.ToInt32() + Detour.Value.OverridenBytes - (LocationOfReturnJump.ToInt32() + 4));
+                if(!ExtensionMethods.WriteBytes(process, LocationOfReturnJump, backAddy))
+                {
+                    return CodeInjectionResult.FailedToWriteInstructionToMemory;
+
+                }
+            }
+
+            //Replace temp addresses with actual position of a variable
+            foreach(var variable in Container.Variables)
+            {
+                foreach(var variableUse in variable.Value.VariableUsage)
+                {
+                    if(!ExtensionMethods.WriteBytes(process, IntPtr.Add(alocAdress, variableUse.ToInt32()), BitConverter.GetBytes(alocAdress.ToInt32() + variable.Value.VariableLocation.ToInt32())))
+                        return CodeInjectionResult.FailedToWriteInstructionToMemory;
+                }
+            }
+
+            foreach(var Detour in Container.Detours)
+            {
+                byte[] DetourInstruction = new byte[Detour.Value.OverridenBytes];
+                DetourInstruction[0] = 0xE9; //Jmp
+                for(int i=5; i<Detour.Value.OverridenBytes; i++)
+                {
+                    DetourInstruction[i] = 0x90; //Nop
+                }
+
+                IntPtr LocationOfDetourJMP = Detour.Value.InjectionPoint;
+                byte[] backAddy = BitConverter.GetBytes(alocAdress.ToInt32() + Detour.Value.MasterContainerStartLocation.ToInt32() - (Detour.Value.InjectionPoint.ToInt32() +5));
+                backAddy.CopyTo(DetourInstruction, 1);
+                if(!ExtensionMethods.WriteBytes(process, LocationOfDetourJMP, DetourInstruction))
+                    return CodeInjectionResult.FailedToWriteInstructionToMemory;
+            }
+            
+            return CodeInjectionResult.Success;
+        }
+
+        /// <summary>
+        /// Calculates and returns the Variable adress.
+        /// </summary>
+        /// <param name="Name">The name of the variable to return.</param>
+        /// <param name="Absolute">Specifies whatever the function should return relative adress or absolute one.</param>
+        /// <returns>Adress of the variable in process' memory</returns>
+        public IntPtr GetVariableAdress(string Name, bool Absolute = false)
+        {
+            if(!Container.Variables.Keys.Contains(Name))
+                throw new Exception("No variable to access!");
+
+            IntPtr offset = Container.Variables[Name].VariableLocation;
+            if(Absolute)
+                return IntPtr.Add(alocAdress, offset.ToInt32());
+            else
+                return IntPtr.Add(alocAdress, offset.ToInt32()) - process.MainModule.BaseAddress.ToInt32();
+        }
+
+        #region StaticStuffToHelp
+        /// <summary>
+        /// Converts bytes as string to byte array.
+        /// </summary>
+        /// <param name="bytesAsString">Bytes as string you want to convert to an array, they should be provided without "0x". Whitespaces are allowed. Throws an exception, on error.</param>
+        /// <returns>Array of bytes.</returns>
+        public static byte[] StringBytesToArray(string bytesAsString)
+        {
+            bytesAsString = bytesAsString.Replace(" ", "");
+            
+            if (bytesAsString.Length % 2 != 0)
+            {
+                throw new Exception("Provided string to conversion does not contain proper amount of bits.");
+            }
+
+            byte[] outArray = new byte[bytesAsString.Length / 2];
+            for (int i = 0; i < outArray.Length; i++)
+            {
+                byte temp = (byte)(CharToByte(bytesAsString[i * 2])*16 + CharToByte(bytesAsString[i * 2 +1]));
+                outArray[i] = temp;
+            }
+            return outArray;
+        }
+
+        private static byte CharToByte(char c)
+        {
+            c = char.ToLower(c);
+            switch (c)
+            {
+                case '0': return 0;
+                case '1': return 1;
+                case '2': return 2;
+                case '3': return 3;
+                case '4': return 4;
+                case '5': return 5;
+                case '6': return 6;
+                case '7': return 7;
+                case '8': return 8;
+                case '9': return 9;
+                case 'a': return 10;
+                case 'b': return 11;
+                case 'c': return 12;
+                case 'd': return 13;
+                case 'e': return 14;
+                case 'f': return 15;
+            }
+            throw new FormatException("Invalid char in a string");
+        }
+        #endregion
+    }
+}
---
+++ CodeInjection/LiveSplit/SuisCodeInjectionContainer.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SuisCodeInjection
+{
+
+    class CodeInjectionMasterContainer
+    {
+        /// <summary>
+        /// A struct for each detour.
+        /// </summary>
+        public struct DetoursStruct
+        {
+            public IntPtr InjectionPoint { get; private set; }
+            public byte OverridenBytes { get; set; }
+            public IntPtr MasterContainerStartLocation { get; private set; }
+            public IntPtr MasterContainerJmpBackLocation { get; set; }
+
+            public DetoursStruct(IntPtr InjectionPoint, byte OverridenBytes, IntPtr MasterContainerStartLocation)
+            {
+                this.InjectionPoint = InjectionPoint;
+                this.OverridenBytes = OverridenBytes;
+                this.MasterContainerStartLocation = MasterContainerStartLocation;
+                this.MasterContainerJmpBackLocation = (IntPtr)0;
+            }
+
+
+            public DetoursStruct(IntPtr InjectionPoint, byte OverridenBytes, IntPtr MasterContainerStartLocation, IntPtr MasterContainerJmpBackLocation)
+            {
+                this.InjectionPoint = InjectionPoint;
+                this.OverridenBytes = OverridenBytes;
+                this.MasterContainerStartLocation = MasterContainerStartLocation;
+                this.MasterContainerJmpBackLocation = MasterContainerJmpBackLocation;
+            }
+        }
+
+        /// <summary>
+        /// A struct for each Variable
+        /// </summary>
+        public struct VariableStruct
+        {
+            public IntPtr VariableLocation { get; private set; }
+            public int VariableLenght { get; private set; }
+            public List<IntPtr> VariableUsage { get; private set; }
+
+            public VariableStruct(IntPtr VariableLocation, int VariableLenght)
+            {
+                this.VariableLocation = VariableLocation;
+                this.VariableLenght = VariableLenght;
+                VariableUsage = new List<IntPtr>();
+            }
+        }
+
+        private List<byte> ByteOpCodes { get; set; }
+        public Dictionary<string, VariableStruct> Variables { get; private set; }
+        public Dictionary<string, DetoursStruct> Detours { get; private set; }
+
+        /// <summary>
+        /// Constructor for CodeInjectionMasterContainer object.
+        /// </summary>
+        public CodeInjectionMasterContainer()
+        {
+            ByteOpCodes = new List<byte>();
+            Variables = new Dictionary<string, VariableStruct>();
+            Detours = new Dictionary<string, DetoursStruct>();
+        }
+
+        /// <summary>
+        /// Gets ByteCode representation.
+        /// </summary>
+        /// <returns>Returns byte op codes as array.</returns>
+        public byte[] GetBytes()
+        {
+            return ByteOpCodes.ToArray();
+        }
+
+        #region VariablesHandling
+        /// <summary>
+        /// Adds a variable to allocate to CodeInjectionMasterContainer. WARNING: Make sure not to allocate variable inside the injection block!
+        /// </summary>
+        /// <param name="Name">Name of the variable, by which you can referance it.</param>
+        /// <param name="Value">The default value to allocate.</param>
+        public void AddVariable(string Name, int Value)
+        {
+            if(Variables.Keys.Contains(Name))
+                throw new Exception("Variable under this name is already specified");
+
+            Variables.Add(Name, new VariableStruct((IntPtr)ByteOpCodes.Count, 4));
+            ByteOpCodes.AddRange(BitConverter.GetBytes(Value));
+        }
+
+        /// <summary>
+        /// Gets the offset variable under specified name.
+        /// </summary>
+        /// <param name="Name">Name of the variable.</param>
+        /// <returns>Returns VariableStruct.</returns>
+        public VariableStruct GetVariableOffset(string Name)
+        {
+            if(Variables.Keys.Contains(Name))
+            {
+                return Variables[Name];
+            }
+            else
+                throw new Exception("Variable under this name wasn't specified");
+        }
+
+        /// <summary>
+        /// Adds an instruction to write to the variable.
+        /// </summary>
+        /// <param name="name">Name of the variable.</param>
+        /// <param name="Value">What to write to the variable.</param>
+        public void AddWriteToVariable(string name, int Value)
+        {
+            if(!Variables.Keys.Contains(name))
+                throw new Exception("No variable specified");
+            else
+            {
+                ByteOpCodes.AddRange(new byte[] { 0xC7, 0x05 });  //mov to absolute address, value
+                Variables[name].VariableUsage.Add((IntPtr)ByteOpCodes.Count);
+                ByteOpCodes.AddRange(new byte[] { 0xFF, 0xFF, 0xFF, 0xFF });  //address
+                ByteOpCodes.AddRange(BitConverter.GetBytes(Value));  //value
+            }
+        }
+
+        public void AddIncrementValue(string name, byte Value)
+        {
+            if(!Variables.Keys.Contains(name))
+                throw new Exception("No variable specified");
+            else
+            {
+                ByteOpCodes.AddRange(new byte[] { 0x83, 0x05 });  //mov to absolute address, value
+                Variables[name].VariableUsage.Add((IntPtr)ByteOpCodes.Count);
+                ByteOpCodes.AddRange(new byte[] { 0xFF, 0xFF, 0xFF, 0xFF });  //address
+                ByteOpCodes.Add(Value);  //value
+            }
+        }
+        #endregion
+
+        #region DetoursCreationAndCode
+        /// <summary>
+        /// Adds an injection point.
+        /// </summary>
+        /// <param name="Name">Name of the injection, by which it can be referenced.</param>
+        /// <param name="InjectionPoint">Adress where to create a detour.</param>
+        /// <param name="OverridenBytes">Amount of bytes (min 5).</param>
+        public void AddInjectionPoint(string Name, IntPtr InjectionPoint, byte OverridenBytes)
+        {
+            if(Detours.Keys.Contains(Name))
+                throw new Exception("Detour under the name " + Name + " already exists!");
+            if(OverridenBytes < 5)
+                throw new Exception("To create a detour, at least 5 bytes have to be overriden (1 for JMP and 4 for offset).");
+
+            Detours.Add(Name, new DetoursStruct(InjectionPoint, OverridenBytes, (IntPtr)ByteOpCodes.Count));
+        }
+
+        /// <summary>
+        /// Adds raw byte code.
+        /// </summary>
+        /// <param name="code">Assembly code as byte OP code.</param>
+        public void AddByteCode(byte[] code)
+        {
+            ByteOpCodes.AddRange(code);
+        }
+
+        /// <summary>
+        /// Closed the injection point, creating the jump back to a spot from which the injection started.
+        /// </summary>
+        /// <param name="Name">Name of the injection point.</param>
+        public void CloseInjection(string Name)
+        {
+            if(Detours.Keys.Contains(Name))
+            {
+                var oldDetour = Detours[Name];
+                Detours[Name] = new DetoursStruct(oldDetour.InjectionPoint, oldDetour.OverridenBytes, oldDetour.MasterContainerStartLocation, (IntPtr)ByteOpCodes.Count +1);
+                ByteOpCodes.Add(0xE9);
+                ByteOpCodes.AddRange(new byte[] { 0xFF, 0xFF, 0xFF, 0xFF });
+            }
+            else
+                throw new Exception("No detour available to close under this name: " + Name);
+        }
+        #endregion
+    }
+}
